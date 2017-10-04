4e94bf448c809a3c820d01c5444007a474652a5e
Ewa Gasperowicz <devnook@gmail.com>
2017-10-04 14:46:47 +0200

    Adding generator for products for firebase

--- src/server/js/models/pie.js
+++ src/server/js/models/pie.js
@@ -23,23 +23,61 @@ const ingredients = require('../../../../data/ingredients.json');
 
 class Pie {
   constructor(topping) {
-    this.DEFAULT_TOPPING = 'icing';
+    this.DEFAULTS = {
+      'topping': Object.keys(ingredients.topping)[0],
+      'dough': Object.keys(ingredients.dough)[0],
+      'filling': Object.keys(ingredients.filling)[0],
+      'decoration': null,
+    };
     this._colors = {};
+    this._attrs = Object.assign({
+      'price': null,
+      'weight': null,
+      'calories': null,
+      'allergens': null,
+      'img': null,
+    }, this.DEFAULTS);
   }
   set template(svgString) {
     this._template = hbs.compile(svgString);
   }
   set topping(topping) {
-    this._topping = ingredients.topping[topping] ?
-      topping : this.DEFAULT_TOPPING;
-    this._colors.topping = ingredients.topping[this._topping];
+    this._attrs.topping = ingredients.topping[topping] ?
+      topping : this.DEFAULTS.topping;
+    this._colors.topping = ingredients.topping[this._attrs.topping];
     this._colors.toppingDark = this._darken(this._colors.topping);
+  }
+  set price(price) {
+    this._attrs.price = isNaN(price) ? this._attrs.price : price;
+  }
+  set weight(weight) {
+    this._attrs.weight = isNaN(weight) ? this._attrs.weight : weight;
+  }
+  set calories(calories) {
+    this._attrs.calories = isNaN(calories) ? this._attrs.calories : calories;
+  }
+  set allergens(allergens) {
+    this._attrs.allergens = Array.isArray(allergens) ?
+      allergens: this._attrs.allergens;
   }
   _darken(sourceColor) {
     return color(sourceColor).darken(0.2).hsl().string();
   }
   svg() {
     return this._template(this._colors);
+  }
+  json() {
+    return Object.assign({
+      'signature': this.signature(),
+    }, this._attrs);
+  }
+  signature() {
+    return [
+      this._attrs.topping || 'x',
+      this._attrs.filling || 'x',
+      this._attrs.dough || 'x',
+      this._attrs.decoration || 'x',
+    ].join('-');
   }
 }
 
