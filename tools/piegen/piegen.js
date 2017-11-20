ba4d3570606f6302fd3f25c385b2b4b71bc4efa3
Ewa Gasperowicz <devnook@gmail.com>
2017-11-20 12:46:20 +0100

    Moving piegen to tools dir

--- tools/piegen/piegen.js
+++ tools/piegen/piegen.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-const Pie = require('../server/js/models/pie.js');
+const Pie = require('./pie.js');
 const ingredients = require('../../data/ingredients.json');
 
 function getRandomKey(obj) {
