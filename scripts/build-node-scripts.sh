a3b65a177fc68270cd9da420c6779ac10899a641
Sérgio Gomes <sgomes@google.com>
2017-11-23 14:08:42 +0000

    Rebasing on top of latest master

--- scripts/build-node-scripts.sh
+++ scripts/build-node-scripts.sh
@@ -6,5 +6,6 @@ mkdir -p $OUTDIR/server/js &&
 mkdir -p $OUTDIR/shared/js &&
 # Change into server directory to pick up server Babel config.
 cd src/server &&
-$BABEL --minified -s --compact=true --out-dir=$OUTDIR/server/js js &&
-$BABEL --minified -s --compact=true --out-dir=$OUTDIR/shared/js ../shared/js
+$BABEL --minified -s --compact=true --out-dir=$OUTDIR/server/js js
+# TODO: Add back in once we have shared code.
+#&& $BABEL --minified -s --compact=true --out-dir=$OUTDIR/shared/js ../shared/js
