b2800f3d65e2c8e0c55d689d0c3375dee99413e1
Ewa Gasperowicz <devnook@gmail.com>
2018-03-16 12:59:52 +0100

    Small css tweaks

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -20,7 +20,7 @@
  import lazyImg from './lazy-img.js';
 
  function initializeIndex() {
-   console.log('aaaaaaaaaaaa')
+   console.log('TODO: Initializing index.')
  }
 
  export default function init() {
