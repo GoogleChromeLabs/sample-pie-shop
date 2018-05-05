1856701aa09cffe152842b4dfc3adfd6416f8273
Sam Dutton <samdutton@gmail.com>
2018-05-05 07:31:17 -0700

    Fixed JavaScript for setting sizes attribute

--- src/client/js/lazy-img.js
+++ src/client/js/lazy-img.js
@@ -20,14 +20,11 @@
 
 const BASE_URL = 'https://res.cloudinary.com/pieshop/f_auto,dpr_auto,q_auto:eco/';
 
-// const SIZES = {
-//   0: 'calc(100vw - 60px)',
-//   420: 'calc((100vw - 90px) / 2)',
-//   750: 'calc((100vw - 120px) / 3)',
-//   1200: 'calc((100vw - 150px) / 4)'
-// }
+const BREAKPOINTS = [0, 420, 750, 1200];
+const DISPLAY_WIDTHS = ['calc(100vw - 60px)', 'calc((100vw - 90px) / 2)', 
+  'calc((100vw - 120px) / 3)', 'calc((100vw - 150px) / 4)']
 
-const WIDTHS = [500, 1000, 1500];
+const IMAGE_WIDTHS = [500, 1000, 1500];
 
 const options = {
   // rootMargin: top, right, bottom, left margins
@@ -52,20 +49,26 @@ function callback(entries) {
 
 function getSrcset(id) {
   const srcset = [];
-  for (const width of WIDTHS) {
+  for (const width of IMAGE_WIDTHS) {
     srcset.push(`${BASE_URL}w_${width}/${id}.jpg ${width}w`);
   }
   return srcset.join(',');
 }
 
 function getSizes() {
-//   const sizes = [];
-//   for (const size in SIZES) { // eslint-disable-line
-//     sizes.push(`(min-width: ${size}px) ${SIZES[size]}`);
-//   }
-//   // return sizes.join(',');
+  const sizes = [];
+  for (let i = 0; i !== BREAKPOINTS.length; ++i) {
+    let size = `(min-width: ${BREAKPOINTS[i]}px) `;
+    const nextBreakpoint = BREAKPOINTS[i + 1];
+    if (nextBreakpoint) {
+      size += `and (max-width: ${nextBreakpoint}px) `;
+    }
+    size += DISPLAY_WIDTHS[i];
+    sizes.push(size);
+  }
+  return sizes.join(',');
 
-  return '(max-width: 419px) calc(100vw - 60px), (min-width: 420px) and (max-width: 750px) calc((100vw - 90px) / 2), (min-width: 750 px) and(max - width: 1200 px) calc((100 vw - 120 px) / 3), (min - width: 1200 px) calc((100 vw - 150 px) / 4)';
+//  return '(max-width: 419px) calc(100vw - 60px), (min-width: 420px) and (max-width: 750px) calc((100vw - 90px) / 2), (min-width: 750 px) and(max - width: 1200 px) calc((100 vw - 120 px) / 3), (min - width: 1200 px) calc((100 vw - 150 px) / 4)';
 }
 
 const images = document.querySelectorAll('img.lazy');
