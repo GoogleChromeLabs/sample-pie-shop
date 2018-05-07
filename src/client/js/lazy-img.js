49206e6fdbbe89cd2ab33bfa6fd53f87ec2ac032
Sam Dutton <samdutton@gmail.com>
2018-05-07 10:38:35 -0700

    Updated menu and shopping cart icons

--- src/client/js/lazy-img.js
+++ src/client/js/lazy-img.js
@@ -21,8 +21,8 @@
 const BASE_URL = 'https://res.cloudinary.com/pieshop/f_auto,dpr_auto,q_auto:eco/';
 
 const BREAKPOINTS = [0, 420, 750, 1200];
-const DISPLAY_WIDTHS = ['calc(100vw - 60px)', 'calc((100vw - 90px) / 2)', 
-  'calc((100vw - 120px) / 3)', 'calc((100vw - 150px) / 4)']
+const DISPLAY_WIDTHS = ['calc(100vw - 60px)', 'calc((100vw - 90px) / 2)',
+  'calc((100vw - 120px) / 3)', 'calc((100vw - 150px) / 4)'];
 
 const IMAGE_WIDTHS = [500, 1000, 1500];
 
@@ -68,7 +68,10 @@ function getSizes() {
   }
   return sizes.join(',');
 
-//  return '(max-width: 419px) calc(100vw - 60px), (min-width: 420px) and (max-width: 750px) calc((100vw - 90px) / 2), (min-width: 750 px) and(max - width: 1200 px) calc((100 vw - 120 px) / 3), (min - width: 1200 px) calc((100 vw - 150 px) / 4)';
+//  return '(max-width: 419px) calc(100vw - 60px), (min-width: 420px) and
+// (max-width: 750px) calc((100vw - 90px) / 2), (min-width: 750 px) and
+// (max - width: 1200 px) calc((100 vw - 120 px) / 3),
+// (min - width: 1200 px) calc((100 vw - 150 px) / 4)';
 }
 
 const images = document.querySelectorAll('img.lazy');
