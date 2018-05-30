471526b081be85f5a577656c956e129f4836e997
Sam Dutton <samdutton@gmail.com>
2018-05-30 14:24:08 +0100

    Cleaned up package.json, removed pie generation code

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -17,8 +17,8 @@
  *
  */
 const fei = require('firestore-export-import');
-fei.initializeApp(process.env.FB_KEYS,
-  'https://firebase.corp.google.com/u/0/project/pie-shop-app/');
+// fei.initializeApp(process.env.FB_KEYS,
+//   'https://firebase.corp.google.com/u/0/project/pie-shop-app/');
 
 import categories from '../../data/categories';
 
--- tasks/build-static-styles.sh
+++ tasks/build-static-styles.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-SASS=node_modules/.bin/node-sass
 OUTDIR=dist/static/styles
 OUTFILE=$OUTDIR/style.css
 INFILE=src/client/styles/style.css
