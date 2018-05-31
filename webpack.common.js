7443e6908d41d5004b6f25696b9bb2b7c34305f2
Sam Dutton <samdutton@gmail.com>
2018-05-31 12:12:21 +0100

    Added stylelint and linted style.css, removed some unused files

--- webpack.common.js
+++ webpack.common.js
@@ -9,10 +9,10 @@ const BROWSERS = [
 
 module.exports = {
   entry: {
-    main: './src/client/js/app.js',
-    listing_main: './src/client/js/listing-main.js',
-    index_main: './src/client/js/index-main.js',
-    category_main: './src/client/js/category-main.js',
+    // main: './src/client/js/app.js',
+    // listing_main: './src/client/js/listing-main.js',
+    // index_main: './src/client/js/index-main.js',
+    // category_main: './src/client/js/category-main.js',
   },
   module: {
     rules: [
