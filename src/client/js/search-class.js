227139307e9a107e2d0da8363154da9c172167b4
Sam Dutton <samdutton@gmail.com>
2018-07-27 19:54:44 +0100

    Basic search working

--- src/client/js/search-class.js
+++ src/client/js/search-class.js
@@ -17,17 +17,21 @@
 *
 */
 
+// defined in /js/third_party/algoliasearchLite.min.js
 /* globals algoliasearch */
+
+import {instance as lazyImg} from './lazy-img.js';
+
+const main = document.querySelector('main');
+const h1 = main.querySelector('h1');
 
 const searchInput = document.querySelector('#search input');
 const searchSvg = document.querySelector('#search svg');
 
 const MIN_QUERY_LENGTH = 3;
 
-// TODO: these values are not secret, but probably don't belong here.
 const ALGOLIA_APP_ID = '3S1LH4M84Y';
-// search-only API key
-const ALGOLIA_API_KEY = 'f5efcc2e1026a8a201e66e1902dc7c07';
+const ALGOLIA_API_KEY = 'f5efcc2e1026a8a201e66e1902dc7c07'; // search-only key
 const ALGOLIA_INDEX_NAME = 'products';
 
 let algoliaIndex;
@@ -40,46 +44,82 @@ class Search {
   }
   doSearch(query) {
     algoliaIndex.search({
+      hitsPerPage: 1000,
       query: query,
-    }, _handleSearchResults);
+    }, _displayResults);
   }
 }
 
 function _addHandlers() {
   searchInput.onkeyup = (event) => {
     if (event.keyCode === 13) {
-      openSearchPage();
+      _openSearchPage();
     }
   };
-  searchSvg.onclick = openSearchPage;
-
-  function openSearchPage() {
-    const query = searchInput.value;
-    if (query.length >= MIN_QUERY_LENGTH) {
-      document.location.href = `/search/${query}`;
-    }
-  }
+  searchSvg.onclick = _openSearchPage;
 }
 
 function _clearSearchInput() {
-  const searchInput = document.querySelector('#search input');
   if (!location.href.includes('search')) {
     searchInput.value = '';
   }
 }
 
-function _handleSearchResults(error, results) {
+function _displayResults(error, results) {
   if (error) {
     throw error;
   }
-  console.log(results.hits);
+  if (results.nbHits === 0) {
+    h1.innerHTML =
+    `Sorry! We didn't find any search results for <em>${results.query}</em>.`;
+  } else {
+    h1.innerHTML =
+      `${results.nbHits} search results for <em>${results.query}</em>`;
+    for (const hit of results.hits) {
+      _displayHit(hit);
+    }
+    lazyImg.loadImages();
+  }
 }
 
+function _displayHit(hit) {
+  const div = document.createElement('div');
+
+  const a = document.createElement('a');
+  a.href = `/${hit.url}`;
+  const img = document.createElement('img');
+  img.alt = hit.name;
+  img.classList.add('lazy');
+  img.classList.add('product');
+  img.crossorigin = 'anonymous';
+  img.dataset.id = hit.id;
+  a.appendChild(img);
+  div.appendChild(a);
+
+  const h2 = document.createElement('h2');
+  h2.textContent = hit.name;
+  div.appendChild(h2);
+
+  const p = document.createElement('p');
+  p.textContent = hit.price;
+  p.classList.add('price');
+  div.appendChild(p);
+
+  main.appendChild(div);
+}
 
 function _initAlgolia() {
   const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
   algoliaIndex = client.initIndex(ALGOLIA_INDEX_NAME);
 }
+
+function _openSearchPage() {
+  const query = searchInput.value;
+  if (query.length >= MIN_QUERY_LENGTH) {
+    document.location.href = `/search/${query}`;
+  }
+}
+
 
 export const search = new Search();
 
